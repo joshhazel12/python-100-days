8c619966fc23e9d09ee1b051555a0f422e8543fa
Rob2164 <robhazel1@gmail.com>
2024-01-29 18:34:28 +0000

    Completed but not quite finished

--- snake_game/main.py
+++ snake_game/main.py
@@ -33,15 +33,13 @@ while game_is_on:
         score_board.increase_score()
 
     # Detect collision with wall.
-    if snake.head.xcor() > 300 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -300:
+    if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() > 295 or snake.head.ycor() < -280:
         game_is_on = False
         score_board.game_over()
 
     # Detect collision with tail
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             score_board.game_over()
     # if head collides with any segment in the tail:
