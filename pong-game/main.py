fb09261adad08b5dd7d2735005a2f8295b30778f
Rob2164 <robhazel1@gmail.com>
2024-01-31 18:50:22 +0000

    Ping pong finished

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,5 +1,8 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 
@@ -8,17 +11,41 @@ screen.screensize(800, 600)
 screen.title("Pong")
 screen.tracer(0)
 
+scoreboard = Scoreboard()
 r_paddle = Paddle((600, 0))
 l_paddle = Paddle((-600, 0))
-
-
+ball = Ball()
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
 
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 517 or ball.ycor() < -517:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (ball.distance(l_paddle) < 50 and ball.xcor() < -320):
+        ball.bounce_x()
+
+    # Detect R paddle misses
+    if ball.xcor() > 615:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect L paddle misses
+    if ball.xcor() < -615:
+        ball.reset_position()
+        scoreboard.r_point()
+
 
 screen.exitonclick()
